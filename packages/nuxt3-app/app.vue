ab2080420a17effbf70911626a0b4584902392ed
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2021-11-09 16:28:45 -0500

    Illustrate import bug

--- packages/nuxt3-app/app.vue
+++ packages/nuxt3-app/app.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
+// This import works
+import message from 'test-lib/test'
+
+// The above import, made in an api function does not work
 const { data } = await useAsyncData('test', () => $fetch('/api/test'))
 </script>
 
 <template>
   <div>
-    Should say hello: {{ data }}
+    Should say "hello hello": "{{ message }} {{ data }}"
   </div>
 </template>
